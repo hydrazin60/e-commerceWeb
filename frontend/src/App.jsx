42620cb956acb43682949cf7f252eeb0502ee9e6
hydrazin60 <sureshkhdk45@gmail.com>
2024-11-02 09:11:31 +0545

    in frontend and server basic configuration setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,5 @@
- import React from 'react'
- 
- export default function App() {
-   return (
-     <div>
-       App
-     </div>
-   )
- }
- 
+import React from "react";
+
+export default function App() {
+  return <div>App</div>;
+}
