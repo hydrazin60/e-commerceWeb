d55842820c31ff6b7308334dc8dea40abebf5e0a
hydrazin60 <sureshkhdk45@gmail.com>
2024-11-02 08:57:07 +0545

    basics project setup

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("DataBase Connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
