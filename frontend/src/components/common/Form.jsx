a24d31ac2464332be6f0d01eaf4879fee082f2eb
hydrazin60 <sureshkhdk45@gmail.com>
2024-11-06 09:04:11 +0545

    Product details Input section Ui create

--- frontend/src/components/common/Form.jsx
+++ frontend/src/components/common/Form.jsx
@@ -10,49 +10,162 @@ import {
 import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 
-export default function CommonForm({
+// export default function CommonForm({
+//   formControls,
+//   formData,
+//   setFormData,
+//   onSubmit,
+//   buttonText,
+// }) {
+//   const renderInputsByComponnentType = (getControlItem) => {
+//     let element = null;
+//     const value = formData[getControlItem.name] || "";
+//     switch (getControlItem.componentType) {
+//       case "input":
+//         element = (
+//           <>
+//             <Input
+//               name={getControlItem.name}
+//               placeholder={getControlItem.placeholder}
+//               type={getControlItem.type}
+//               id={getControlItem.name}
+//               value={value}
+//               onChange={(e) =>
+//                 setFormData({
+//                   ...formData,
+//                   [getControlItem.name]: e.target.value,
+//                 })
+//               }
+//             />
+//           </>
+//         );
+//         break;
+//       case "select":
+//         element = (
+//           <Select
+//             onValueChange={(value) =>
+//               setFormData({ ...formData, [getControlItem.name]: value })
+//             }
+//             value={value}
+//           >
+//             <SelectTrigger className="w-full">
+//               <SelectValue placeholder={getControlItem.placeholder} />
+//             </SelectTrigger>
+//             <SelectContent>
+//               {getControlItem.options && getControlItem.length > 0
+//                 ? getControlItem.options.map((optionItem) => (
+//                     <SelectItem key={optionItem.id} value={optionItem.id}>
+//                       {optionItem.label}
+//                     </SelectItem>
+//                   ))
+//                 : null}
+//             </SelectContent>
+//           </Select>
+//         );
+//         break;
+
+//       case "textarea":
+//         element = (
+//           <Textarea
+//             name={getControlItem.name}
+//             placeholder={getControlItem.placeholder}
+//             id={getControlItem.id}
+//             value={value}
+//             onChange={(e) =>
+//               setFormData({
+//                 ...formData,
+//                 [getControlItem.name]: e.target.value,
+//               })
+//             }
+//           />
+//         );
+//         break;
+
+//       default:
+//         element = (
+//           <Input
+//             name={getControlItem.name}
+//             placeholder={getControlItem.placeholder}
+//             type={getControlItem.type}
+//             id={getControlItem.name}
+//             value={value}
+//             onChange={(e) =>
+//               setFormData({
+//                 ...formData,
+//                 [getControlItem.name]: e.target.value,
+//               })
+//             }
+//           />
+//         );
+//         break;
+//     }
+//     return element;
+//   };
+//   return (
+//     <>
+//       <form onSubmit={onSubmit}>
+//         <div className="flex flex-col gap-3">
+//           {formControls.map((controlItem) => (
+//             <div key={controlItem.name} className="  grid w-full gap-1.5">
+//               {renderInputsByComponnentType(controlItem)}
+//             </div>
+//           ))}
+//         </div>
+//         <Button type="submit" className="mt-2 w-full bg-blue-600 text-md">
+//           {buttonText || "Submit"}
+//         </Button>
+//       </form>
+//     </>
+//   );
+// }
+
+function CommonForm({
   formControls,
   formData,
   setFormData,
   onSubmit,
   buttonText,
+  isBtnDisabled,
 }) {
-  const renderInputsByComponnentType = (getControlItem) => {
+  function renderInputsByComponentType(getControlItem) {
     let element = null;
     const value = formData[getControlItem.name] || "";
+
     switch (getControlItem.componentType) {
       case "input":
         element = (
-          <>
-            <Input
-              name={getControlItem.name}
-              placeholder={getControlItem.placeholder}
-              type={getControlItem.type}
-              id={getControlItem.name}
-              value={value}
-              onChange={(e) =>
-                setFormData({
-                  ...formData,
-                  [getControlItem.name]: e.target.value,
-                })
-              }
-            />
-          </>
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+            value={value}
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: event.target.value,
+              })
+            }
+          />
         );
+
         break;
       case "select":
         element = (
           <Select
             onValueChange={(value) =>
-              setFormData({ ...formData, [getControlItem.name]: value })
+              setFormData({
+                ...formData,
+                [getControlItem.name]: value,
+              })
             }
             value={value}
           >
             <SelectTrigger className="w-full">
-              <SelectValue placeholder={getControlItem.placeholder} />
+              <SelectValue placeholder={getControlItem.label} />
             </SelectTrigger>
             <SelectContent>
-              {getControlItem.options && getControlItem.length > 0
+              {getControlItem.options && getControlItem.options.length > 0
                 ? getControlItem.options.map((optionItem) => (
                     <SelectItem key={optionItem.id} value={optionItem.id}>
                       {optionItem.label}
@@ -62,8 +175,8 @@ export default function CommonForm({
             </SelectContent>
           </Select>
         );
-        break;
 
+        break;
       case "textarea":
         element = (
           <Textarea
@@ -71,14 +184,15 @@ export default function CommonForm({
             placeholder={getControlItem.placeholder}
             id={getControlItem.id}
             value={value}
-            onChange={(e) =>
+            onChange={(event) =>
               setFormData({
                 ...formData,
-                [getControlItem.name]: e.target.value,
+                [getControlItem.name]: event.target.value,
               })
             }
           />
         );
+
         break;
 
       default:
@@ -86,35 +200,38 @@ export default function CommonForm({
           <Input
             name={getControlItem.name}
             placeholder={getControlItem.placeholder}
-            type={getControlItem.type}
             id={getControlItem.name}
+            type={getControlItem.type}
             value={value}
-            onChange={(e) =>
+            onChange={(event) =>
               setFormData({
                 ...formData,
-                [getControlItem.name]: e.target.value,
+                [getControlItem.name]: event.target.value,
               })
             }
           />
         );
         break;
     }
+
     return element;
-  };
+  }
+
   return (
-    <>
-      <form onSubmit={onSubmit}>
-        <div className="flex flex-col gap-3">
-          {formControls.map((controlItem) => (
-            <div key={controlItem.name} className="  grid w-full gap-1.5">
-              {renderInputsByComponnentType(controlItem)}
-            </div>
-          ))}
-        </div>
-        <Button type="submit" className="mt-2 w-full bg-blue-600 text-md">
-          {buttonText || "Submit"}
-        </Button>
-      </form>
-    </>
+    <form onSubmit={onSubmit}>
+      <div className="flex flex-col gap-3">
+        {formControls.map((controlItem) => (
+          <div className="grid w-full gap-1.5" key={controlItem.name}>
+            <label className="mb-1">{controlItem.label}</label>
+            {renderInputsByComponentType(controlItem)}
+          </div>
+        ))}
+      </div>
+      <Button disabled={isBtnDisabled} type="submit" className="mt-2 w-full">
+        {buttonText || "Submit"}
+      </Button>
+    </form>
   );
 }
+
+export default CommonForm;
--- frontend/src/pages/AdminView/Product.jsx
+++ frontend/src/pages/AdminView/Product.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import CommonForm from "@/components/common/Form";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { addProductFormElements } from "@/config";
+import React, { Fragment } from "react";
+const initialFormData = {
+  title: "",
+  description: "",
+  price: "",
+  image: "",
+  category: "",
+  brand: "",
+  salePrice: "",
+  totalStock: "",
+};
 
-export default function  AdminProduct() {
+export default function AdminProduct() {
+  const [openCreateProductDialog, setOpenCreateProductDialog] =
+    React.useState(false);
+  const [formData, setFormData] = React.useState(initialFormData);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
-    <div>
-      AdminProduct
-    </div>
-  )
+    <Fragment>
+      <div className="mb-5 w-full flex justify-end ">
+        <Button
+          onClick={() => setOpenCreateProductDialog(!openCreateProductDialog)}
+        >
+          Add New Product
+        </Button>
+      </div>
+      <div className="grid grid-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet
+        open={openCreateProductDialog}
+        onOpenChange={() => {
+          setOpenCreateProductDialog(!openCreateProductDialog);
+        }}
+      >
+        <SheetContent side="right" className="overflow-hidden">
+          <SheetHeader>
+            <SheetTitle>Add New Product</SheetTitle>
+          </SheetHeader>
+
+          <div className="py-6">
+            <CommonForm
+              onSubmit={onSubmit}
+              formData={formData}
+              setFormData={setFormData}
+              formControls={addProductFormElements}
+              buttonText="Add Product"
+            ></CommonForm>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </Fragment>
+  );
 }
